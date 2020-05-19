21acad3e20216579e95f283ff12dcb07b673edfc
AyanBhadury <ayon.bony@gmail.com>
2020-05-19 22:18:59 +0530

    Updates

---
+++ precache-manifest.add78e72057fddec992153cab909736d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae23f7065fa8ad8330bd0f0f6717d430",
+    "url": "/recipe-finder/index.html"
+  },
+  {
+    "revision": "87905535ddeafd14c5a1",
+    "url": "/recipe-finder/static/css/main.14629c50.chunk.css"
+  },
+  {
+    "revision": "ebd2adb7c4130d1dfa2e",
+    "url": "/recipe-finder/static/js/2.44b10ca6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-finder/static/js/2.44b10ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87905535ddeafd14c5a1",
+    "url": "/recipe-finder/static/js/main.14d84efe.chunk.js"
+  },
+  {
+    "revision": "d00b89d0182a6892e59c",
+    "url": "/recipe-finder/static/js/runtime-main.ef059672.js"
+  }
+]);
